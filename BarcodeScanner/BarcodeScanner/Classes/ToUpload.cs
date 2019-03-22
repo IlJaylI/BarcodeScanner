f9f3acdde2991cd259d5428701a790ecbbd75f07
Tyrone turner <tyrone.turner@avantech.local>
2019-03-22 10:02:04 +0100

    Add project files.

---
+++ BarcodeScanner/BarcodeScanner/Classes/ToUpload.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace BarcodeScanner.Classes
+{
+    class ToUpload
+    {
+        public String Batch { get; set; }
+        public String Client{ get; set; }
+
+
+        //posisible cache overload
+        public Stream Image { get; set; } 
+        //public Byte[] Images { get; set; }
+
+
+        public ToUpload() { }
+
+
+    }
+}
---
+++ BarcodeScanner/BarcodeScanner/MainPage.xaml.cs
@@ -0,0 +1,62 @@
+﻿using Plugin.Media;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BarcodeScanner
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void TakePicture(object sender,EventArgs e)
+        {
+            Classes.ToUpload Data = new Classes.ToUpload();
+
+            //Check internet connection
+
+            string name = Guid.NewGuid().ToString();
+            name += ".jpg";
+            Debug.WriteLine("Filename Creted: "+name);
+
+
+            if(!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
+            {
+                await DisplayAlert("No Camera", "No Camera Avalible", "OK");
+            }
+
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
+                SaveToAlbum = true,
+                Name = name
+            });
+
+            if (file == null)
+                return;
+
+            Data.Image = file.GetStream();
+            Data.Client = Client.Text;
+            Data.Batch = Batch.Text;
+
+            Debug.WriteLine("Batch: " + Data.Batch +"Client: " + Data.Client);
+
+            //add date to object?
+
+
+
+
+
+
+            //Preview
+            //Image1.Source = ImageSource.FromStream(() => file.GetStream());
+        }
+
+    }
+}
